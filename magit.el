c278e861c40033959081336a29a27a7a8c02dfb5
Charlie El Hourani <chachie19@gmail.com>
2023-12-06 17:02:15 +0200

    feat(TKT-1): add magit module with initial helper function

---
+++ magit.el
@@ -0,0 +1,11 @@
+(defun extract-branch-tag (branch-name)
+  "Return conventional commit prefix for git based on BRANCH-NAME.
+Example: for branch bugfix/tkt-123 return tkt-123(bugfix)"
+  (seq-let [v1 v2] (split-string branch-name "/")
+    (format "%s(%s): " v1 (upcase v2))))
+
+(defun git-commit-insert-branch-with-conventional-commit ()
+  "Insert into current buffer a conventional commit prefix."
+  (insert (extract-branch-tag (magit-get-current-branch))))
+
+(add-hook 'git-commit-setup-hook 'git-commit-insert-branch-with-conventional-commit)
